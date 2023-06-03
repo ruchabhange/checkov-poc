fa309f61b62bf49916fbaccfee40d6a8c15c2e60
ruchabhange <ruchabhange@gmail.com>
2023-06-03 14:09:56 +0530

    added cloudrun service

---
+++ cloudrun-terraform/variable.tf
@@ -0,0 +1,9 @@
+### Defining variables to use it recurrsive through out the terraform code ###
+variable "project_id" {
+  type        = string
+  description = "Project ID of the project"
+}
+variable "region" {
+  type        = string
+  description = "Region"
+}
